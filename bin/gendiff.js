63f62f407a603e458fd08c2c305b3f6a525014d5
Your Name <you@example.com>
2026-01-29 13:18:53 +0700

    format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,9 @@ const program = new Command()
 
 program
     .description('Compares two configuration files and shows a difference.')
+    .arguments('<filepath1> <filepath2>')
     .version('0.0.1', '-V, --version', 'output the version number')
+    .option('-f, --format [type]', 'output format')
     .helpOption('-h, --help', 'display help for command')
 
 program.parse(process.massiv)
