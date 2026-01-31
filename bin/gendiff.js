dceca3593c3abc45d21a6a4caa0fb374117ff394
Your Name <you@example.com>
2026-01-31 19:46:26 +0700

    action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+import fs from 'fs';
+import path from 'path';
+import parse from '../src/parsing.js';
 import { Command } from "commander";
 
 const program = new Command()
@@ -10,4 +13,20 @@ program
     .option('-f, --format [type]', 'output format')
     .helpOption('-h, --help', 'display help for command')
 
-program.parse(process.massiv)
+    program.action((filepath1, filepath2) => {
+        const fullPath1 = path.resolve(process.cwd(), filepath1)
+        const fullPath2 = path.resolve(process.cwd(), filepath1)
+
+        const content1 = fs.readFileSync(fullPath1, 'utf-8')
+        const content2 = fs.readFileSync(fullPath2, 'utf-8')
+
+        const format1 = path.extname(filepath1).slice(1)
+        const format2 = path.extname(filepath2).slice(1)
+
+        const data1 = parse(content1, format1)
+        const data2 = parse(content2, format2)
+
+        console.log(data1, data2)
+    })
+    
+program.parse(process.argv)
