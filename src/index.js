0e48b2ef1e8466bf551d3cce27cc5d4a93c33197
Your Name <you@example.com>
2026-02-03 00:59:07 +0700

    genDiff_update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import _ from "lodash";
 
 const genDiff = (data1, data2) => {
+    const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)))
 
+    const lines = keys.map ((key) => {
+        const value1 = data1[key]
+        const value2 = data2[key]
+
+        if (!_.has(data1, key)) {
+            return `  + ${key}: ${value2}`
+        }
+        if (!_.has(data2, key)) {
+            return `  - ${key}: ${value1}`
+        }
+        if (value1 !== value2) {
+            return `  - ${key}: ${value1}\n  + ${key}: ${value2}`
+        }
+
+        return `    ${key}: ${value1}`
+    })
+
+    return `{\n${lines.join('\n')}\n`
 }
 
 export default genDiff
